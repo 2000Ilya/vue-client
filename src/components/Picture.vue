e4b46099309a567136933adc79a341c3fc051e07
Ilya Konik <konick.ilya@yandex.ru>
2023-02-17 22:48:48 +0300

    character card created

---
+++ src/components/Picture.vue
@@ -0,0 +1,21 @@
+<template>
+  <img v-bind:src="imgSrc" class="picture" alt="character" />
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Picture",
+  props: {
+    imgSrc: String,
+  },
+});
+</script>
+
+<style scoped>
+.picture {
+  width: 30rem;
+  height: 30rem;
+}
+</style>
