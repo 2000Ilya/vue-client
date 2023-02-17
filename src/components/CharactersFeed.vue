40e374eebb90a95fae2a2764b1bf9c207487c6a6
Ilya Konik <konick.ilya@yandex.ru>
2023-02-17 22:50:59 +0300

    characters feed created

---
+++ src/components/CharactersFeed.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="characters-feed">
+    <Character
+      v-for="character in characters"
+      v-bind:key="character.id"
+      v-bind:gender="character.gender"
+      v-bind:name="character.name"
+      v-bind:status="character.status"
+      v-bind:origin="character.origin.name"
+      v-bind:image="character.image"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from "vue";
+import Character from "./Character.vue";
+import { Character as CharacterType } from "@/utils/types";
+
+export default Vue.extend({
+  components: {
+    Character,
+  },
+  name: "CharactersFeed",
+  props: {
+    characters: { required: true, type: Array as PropType<CharacterType[]> },
+  },
+});
+</script>
+
+<style scoped>
+.characters-feed-container {
+  margin: auto;
+}
+
+.characters-feed {
+  max-width: 63rem;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 3rem;
+}
+</style>
