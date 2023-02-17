30e5f2f73b237779b9801e0158745253cb8a5324
Ilya Konik <konick.ilya@yandex.ru>
2023-02-17 22:51:34 +0300

    entire app assemled

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,28 @@
 <template>
   <div id="app">
-    <Paginator
-      v-if="pages !== null"
-      v-bind:totalCount="pages.length"
-      v-bind:siblingCount="3"
-      v-bind:currentPage="currentPage"
-      v-bind:pages="pages"
-      v-bind:pagesMiddleRange="
-        currentPage > 2 && pages.length - currentPage > 2
-          ? pages.slice(
-              pages.indexOf(currentPage) - 1,
-              pages.indexOf(currentPage) + 2
-            )
-          : pages.length - currentPage <= 2
-          ? pages.slice(pages.length - 4, pages.length - 1)
-          : pages.slice(1, 4)
-      "
-      v-bind:setPage="setPage"
-    />
+    <div v-if="pages !== null && characters !== null">
+      <CharactersFeed
+        class="app__characters-feed"
+        v-bind:characters="characters"
+      />
+      <Paginator
+        v-bind:totalCount="pages.length"
+        v-bind:siblingCount="3"
+        v-bind:currentPage="currentPage"
+        v-bind:pages="pages"
+        v-bind:pagesMiddleRange="
+          currentPage > 2 && pages.length - currentPage > 2
+            ? pages.slice(
+                pages.indexOf(currentPage) - 1,
+                pages.indexOf(currentPage) + 2
+              )
+            : pages.length - currentPage <= 2
+            ? pages.slice(pages.length - 4, pages.length - 1)
+            : pages.slice(1, 4)
+        "
+        v-bind:setPage="setPage"
+      />
+    </div>
     <div v-else><Loader /></div>
   </div>
 </template>
@@ -25,6 +30,7 @@
 <script lang="ts">
 import Vue from "vue";
 import Paginator from "@/components/Paginator.vue";
+import CharactersFeed from "@/components/CharactersFeed.vue";
 import Loader from "@/components/Loader.vue";
 import apiCharacterRequest from "./utils/apiCharacterRequest";
 import { Character, DataInfo } from "./utils/types";
@@ -33,13 +39,14 @@ export default Vue.extend({
   components: {
     Paginator,
     Loader,
+    CharactersFeed,
   },
   data() {
     return {
       pages: null as number[] | null,
       characters: null as Character[] | null,
       currentPage: 1,
-      info: null as DataInfo | null,
+      // info: null as DataInfo | null,
     };
   },
   methods: {
@@ -49,7 +56,7 @@ export default Vue.extend({
     getData: function () {
       apiCharacterRequest(this.currentPage).then((response) => {
         this.characters = response.data.results;
-        this.info = response.data.info;
+        // this.info = response.data.info;
 
         this.pages = Array(response.data.info.pages)
           .fill(null)
@@ -67,3 +74,40 @@ export default Vue.extend({
   },
 });
 </script>
+
+<style>
+@font-face {
+  font-family: "Roboto Condensed";
+  font-weight: 400;
+  src: local("Roboto Condensed"),
+    url(./assets/fonts/RobotoCondensed-Regular.ttf) format("truetype");
+}
+@font-face {
+  font-family: "Roboto Condensed";
+  font-weight: 700;
+  src: local("Roboto Condensed"),
+    url(./assets/fonts/RobotoCondensed-Bold.ttf) format("truetype");
+}
+
+html {
+  font-size: 10px;
+}
+
+body {
+  margin: 0;
+  font-family: "Roboto Condensed";
+  font-weight: 400;
+}
+
+button {
+  outline: none;
+  border: none;
+  background: none;
+  cursor: pointer;
+  padding: 0;
+}
+
+.app__characters-feed {
+  margin: 1.6rem auto 7.6rem;
+}
+</style>
