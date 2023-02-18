179e779879244a04363cf7bce1a7a495ef719893
Ilya Konik <konick.ilya@yandex.ru>
2023-02-18 04:28:14 +0300

    styles corrected and smooth appearance animation added

--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -1,21 +1,66 @@
 <template>
-  <img v-bind:src="imgSrc" class="picture" alt="character" />
+  <div class="picture-container">
+    <img
+      v-show="isLoaded"
+      v-bind:src="imgSrc"
+      class="picture"
+      alt="character"
+      @load="onImgLoad"
+    />
+    <Loader class="picture-loader" v-if="!isLoaded" />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import Loader from "./Loader.vue";
 
 export default Vue.extend({
   name: "Picture",
+  components: {
+    Loader,
+  },
   props: {
     imgSrc: String,
+  },
+  data() {
+    return {
+      isLoaded: false,
+    };
+  },
+  methods: {
+    onImgLoad() {
+      this.isLoaded = true;
+    },
   },
 });
 </script>
 
 <style scoped>
-.picture {
+.picture-container {
+  position: relative;
   width: 30rem;
   height: 30rem;
+}
+
+.picture,
+.picture-loader {
+  width: 100%;
+  height: 100%;
+}
+
+.picture {
+  object-fit: contain;
+  animation-name: appearance;
+  animation-duration: 1s;
+}
+
+@keyframes appearance {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
 }
 </style>
